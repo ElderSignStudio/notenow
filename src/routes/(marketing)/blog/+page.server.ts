f485cc5c9b77c93bf8abd43b1851e4d5efc8465f
Dan Eisenberg <dev@eisenbergdan.com>
2026-01-23 14:38:54 +0100

    Shows how to wait for parent to complete before another load runs

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,7 +1,16 @@
 import type { PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({parent}) => {
+
+	// First do stuff that does not depend on parent
+
+	const parentData = await parent();
+
+	// ... then do stuff that depends on parent
+
+	console.log(parentData);
 	console.log('🌍 Blog Route Server Load');
+
 	return {
 		title: 'The Blog',
 		count: 10
