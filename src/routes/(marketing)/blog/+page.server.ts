56a0db1070d4bc858ce51ad864ae994db3b98bf4
Dan Eisenberg <dev@eisenbergdan.com>
2026-01-28 15:31:37 +0100

    Shows how to use paginations as an url parameter

--- src/routes/(marketing)/blog/+page.server.ts
+++ src/routes/(marketing)/blog/+page.server.ts
@@ -1,10 +1,12 @@
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import type { PostsResponse } from '$lib/types';
+import { POSTS_PER_PAGE } from '$lib/constants';
 
-export const load = (async ({fetch}) => {
+export const load = (async ({fetch, url}) => {
 
-	const postsRes = await fetch('/api/posts');
+	const page = +(url.searchParams.get('page') || 1)
+	const postsRes = await fetch(`https://dummyjson.com/posts?limit=${POSTS_PER_PAGE}&skip=${(page - 1) * POSTS_PER_PAGE}`);
 
 	if(!postsRes.ok) {
 		error(postsRes.status, 'An error has occurred!')
