c3741bbca1944874554d2845cf6966f5f3bfd578
Dan Eisenberg <dev@eisenbergdan.com>
2026-01-29 15:46:06 +0100

    Improves loading times by running loading post and comments concurrently

--- src/routes/(marketing)/blog/[id]/+page.server.ts
+++ src/routes/(marketing)/blog/[id]/+page.server.ts
@@ -21,8 +21,10 @@ export const load = (async ({params, fetch}) => {
         return commentsArray;
     }
 
+    const [post, comments] = await Promise.all([fetchPost(), fetchComments()]);
+
     return {
-        post: await fetchPost(),
-        comments: await fetchComments()
+        post,
+        comments
     };
 });
