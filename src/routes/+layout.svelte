002467b112d7c0ad56e2b958b850e0057cac8dd9
Dan Eisenberg <dev@eisenbergdan.com>
2026-01-29 23:47:24 +0100

    Adds a progress bar module that shows if takes more than 500ms

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,33 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { page } from '$app/state';
+	import { BProgress } from '@bprogress/core';
+	import '@bprogress/core/css';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
 
+	BProgress.configure({ showSpinner: false });
 	let { children } = $props();
+
+	let loadingTimeout: number;
+
+	beforeNavigate(() => {
+		loadingTimeout = window.setTimeout(() => {
+			BProgress.start();
+		}, 500);
+	});
+
+	afterNavigate(() => {
+		window.clearTimeout(loadingTimeout);
+		BProgress.done();
+	});
 </script>
 
 <svelte:head>
 	<title>{page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}</title>
-	<meta property="og:title" content={page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'} />
+	<meta
+		property="og:title"
+		content={page.data.title ? `${page.data.title} | NoteNow` : 'NoteNow'}
+	/>
 	{#if page.data.description}
 		<meta property="og:description" content={page.data.description} />
 		<meta name="description" content={page.data.description} />
