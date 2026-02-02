68591c6392a15a3e105a7f3f3b55be2e58ed659b
Dan Eisenberg <dev@eisenbergdan.com>
2026-02-03 00:07:14 +0100

    Uses locals in the hook to to check cookie if user is logged
    If there's no cookie (checked in the hook), redirect to sign in. If there is, add the user to loocals and resolves. Locals with the user will be available in the load function (in this case in the layout).

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,12 +2,16 @@ import { redirect, type Handle } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 
 export const handle1: Handle = async ({event, resolve}) => {
-    if (event.url.pathname.startsWith('/posts')) {
-        redirect(308, '/blog');
-        // return new Response('custom response')
+
+    const token = event.cookies.get('token');
+    if (event.url.pathname.startsWith('/app') && !token) {
+        throw redirect(307, '/signin');
     }
+
+    event.locals.user = token ? {name: 'John', id: 1} : null;
+
     const response = await resolve(event);
     return response;
-}
+};
 
 export const handle = sequence(handle1);
